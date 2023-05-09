932c971bedf8d202db3188250f7803215ee0281f
Amy Snarr <amy.snarr16@gmail.com>
2023-05-09 13:16:07 -0600

    displays content on page

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Main from "./pages/Main"
+
+export default function App() {
+    console.log('Rendering App');
+    return <Main />
+}
---
+++ client/src/pages/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Main() {
+
+    return (
+        <p>HELLO?</p>
+    )
+}
+
+export default Main;
